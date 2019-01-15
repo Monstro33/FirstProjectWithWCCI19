e8609b8c4b955651e14750aa21ee90bd67835f5e
Michael Manville <monstro33@yahoo.com>
2019-01-15 11:36:17 -0500

    Initial Commit

---
+++ HelloWorldSpring19.Test/ProgramTest.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace HelloWorldSpring19.Test
+{
+    public class ProgramTest
+    {
+        [Fact]
+        public void Hello_World_Test()
+        {
+            Assert.Equal("Hello", "Hello"); 
+        } //This is a comment
+    }
+}
---
+++ HelloWorldSpring19.Tests/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HelloWorldSpring19.Tests
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
